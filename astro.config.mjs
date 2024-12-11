855a43861f6dbf85affd14cf652129b85e3041a9
Phudit Chuengjaroen <phudit.nut@gmail.com>
2024-12-11 23:27:48 +0700

    add config for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://nuttikung.github.io',
+  base: 'example-astro',
   integrations: [tailwind()]
 });
